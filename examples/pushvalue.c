d6eb187bc7cdd3096a8b03e6cbeb29721bfac0f5
Wei Ning <nwcgnw@gmail.com>
2017-12-18 12:01:49 +1300

    - add more example cases in pushvalue.c, showing how to interact with the stack

--- examples/pushvalue.c
+++ examples/pushvalue.c
@@ -4,6 +4,7 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <string.h>
 
 #include <lua.h>
@@ -23,9 +24,7 @@ void demoCheckStack(struct lua_State *pState) {
 
 
 
-void demoPushAndQueryValues(struct lua_State *pState) {
-    int error = 0;
-
+void demoPushAndQueryValueTypes(struct lua_State *pState) {
     const char *str = "IDFKA";
     lua_Number aDouble = 3.14;
     lua_Integer anInt = 0xDEAD;
@@ -34,11 +33,107 @@ void demoPushAndQueryValues(struct lua_State *pState) {
     assert(lua_isnil(pState, -1));
     assert(!lua_isinteger(pState, -1));
     assert(!lua_isstring(pState, -1));
+    lua_pop(pState, 1);
 
-    if (error) {
-        fprintf(stderr, "%s\n", lua_tostring(pState, -1));
-        lua_pop(pState, 1);
+    lua_pushnumber(pState, aDouble);
+    assert(lua_isnumber(pState, -1));
+    assert(lua_isstring(pState, -1));
+    lua_pop(pState, 1);
+
+    lua_pushstring(pState, str);
+    assert(lua_isstring(pState, -1));
+    lua_pop(pState, 1);
+}
+
+
+void demoPushAndQueryValues(struct lua_State *pState) {
+    int canConvertToValue = 0;
+    const char *str = "IDDQD";
+    const char *oStr = NULL;
+    size_t oStrLength = 0;
+
+    lua_pushboolean(pState, 1);
+    assert(1 == lua_toboolean(pState, -1));
+    lua_pop(pState, 1);
+
+    lua_pushinteger(pState, 0xDEAD);
+    assert(0xDEAD == lua_tointegerx(pState, -1, &canConvertToValue));
+    assert(canConvertToValue);
+    lua_pop(pState, 1);
+
+    lua_pushnumber(pState, 3.14);
+    lua_tonumberx(pState, -1, &canConvertToValue);
+    assert(canConvertToValue);
+    lua_pop(pState, 1);
+
+    lua_pushstring(pState, str);
+    oStr = lua_tolstring(pState, -1, &oStrLength);
+    assert(5 == oStrLength);
+    assert(0 == strcmp(str, oStr));
+    lua_pop(pState, 1);
+
+    lua_pushstring(pState, str);
+    oStr = lua_tostring(pState, -1);
+    assert(0 == strcmp(str, oStr));
+    lua_pop(pState, 1);
+}
+
+
+void demoPushAndClear(struct lua_State *pState) {
+    assert(0 == lua_gettop(pState));
+
+    for (int i=0; i < 10; ++i) {
+        lua_pushinteger(pState, 0xBEEF);
     }
+    assert(10 == lua_gettop(pState));
+
+    lua_pop(pState, 1);
+    assert(9 == lua_gettop(pState));
+
+    // pop everything
+    lua_pop(pState, -1);
+    assert(0 == lua_gettop(pState));
+
+    for (int i=0; i < 10; ++i) {
+        lua_pushnumber(pState, 3.14);
+    }
+    assert(10 == lua_gettop(pState));
+
+    // explicitly pop everything, specifying the length
+    lua_pop(pState, 10);
+    assert(0 == lua_gettop(pState));
+}
+
+// TODO: re-read C pointer Book about how c-string works!
+// char oStrAll[] = {} will cause segfault, it has to have a length
+// GCC message: stack smashing detected
+void demoPushAndIterate(struct lua_State *pState) {
+    int top;
+    int valueType;
+    size_t oStrLen;
+    const char *oStr = NULL;
+    char oStrAll[16] = {};
+    lua_pushinteger(pState, 1);
+    lua_pushnumber(pState, 3.0);
+    lua_pushstring(pState, ".");
+    lua_pushnumber(pState, 3.14);
+    top = lua_gettop(pState);
+    for (int idx = 1; idx <= top; ++idx) {
+        valueType = lua_type(pState, idx);
+        oStrLen = 0;
+        switch (valueType) {
+            case LUA_TNUMBER: {
+                oStr = lua_tolstring(pState, idx, &oStrLen);
+            }
+            default:
+                ;
+        }
+        if (oStrLen) {
+            strcat(oStrAll, oStr);
+        }
+    }
+    assert(0 == strcmp("13.03.14", oStrAll));
+    lua_pop(pState, -1);
 }
 
 
@@ -47,7 +142,10 @@ int main(int argc, char **argv) {
     luaL_openlibs(pState);
 
     demoCheckStack(pState);
+    demoPushAndQueryValueTypes(pState);
     demoPushAndQueryValues(pState);
+    demoPushAndClear(pState);
+    demoPushAndIterate(pState);
 
     lua_close(pState);
     return 0;
