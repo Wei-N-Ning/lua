0484c13a97760a28da3dc1f7c7519efe78813345
Wei Ning <nwcgnw@gmail.com>
2017-12-18 19:22:34 +1300

    - complete luaconfigurator.cpp example
    this example shows that using Lua as the configuration language, one can create "smart" configuration that builds business logic in

--- examples/luaconfigurator.cpp
+++ examples/luaconfigurator.cpp
@@ -4,12 +4,45 @@
 
 #include "luaconfigurator.h"
 
-#include <iostream>
+#include <cassert>
+#include <string>
+
+
+int getInteger(struct lua_State *pState, const char *name) {
+    int isnum = 0;
+    int result = 0;
+    lua_getglobal(pState, name);
+    result = (int)lua_tointegerx(pState, -1, &isnum);
+    lua_pop(pState, 1);
+    return result;
+}
+
+
+bool loadConfig(struct lua_State *pState) {
+    std::string config(
+        "IDKFA = 200\n"
+        "IDDQD = 300\n"
+        "IDNOCLIP = -400\n"
+        "local function init()\n"
+            "IDNOCLIP = 400\n"
+        "end\n"
+        "init()\n"
+    );
+    if (luaL_loadstring(pState, config.c_str()) || (lua_pcall(pState, 0, 0, 0))) {
+        return false;
+    };
+    return true;
+}
 
 
 int main(int argc, char **argv) {
     struct lua_State *pState = luaL_newstate();
     luaL_openlibs(pState);
+
+    assert(loadConfig(pState));
+    assert(200 == getInteger(pState, "IDKFA"));
+    assert(300 == getInteger(pState, "IDDQD"));
+    assert(400 == getInteger(pState, "IDNOCLIP"));
 
     lua_close(pState);
     return 0;
