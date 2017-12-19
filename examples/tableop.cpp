21f8a032ad3edcbca0317edda36311136f2bcdf2
Wei Ning <nwcgnw@gmail.com>
2017-12-19 13:20:05 +1300

    - add tableop.cpp example showing how to interface with Lua table via the C API;
    - there is a pattern in designing the C/C++ getter functions

---
+++ examples/tableop.cpp
@@ -0,0 +1,96 @@
+//
+// Created by wein on 12/18/17.
+//
+
+#include "tableop.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+
+bool loadTable(struct lua_State *pState, const char *sourceCode, const char *tableName) {
+    if (luaL_loadstring(pState, sourceCode) || lua_pcall(pState, 0, 0, 0)) {
+        std::cout << "can not load string" << std::endl;
+        return false;
+    }
+    lua_getglobal(pState, tableName);
+    if (lua_istable(pState, -1)) {
+        return true;
+    }
+    std::cout << "invalid table (lua_istable())" << std::endl;
+    return false;
+}
+
+
+
+std::string getStringField(struct lua_State *pState, const char *name) {
+    size_t len = 0;
+    const char *field = nullptr;
+    lua_pushstring(pState, name);
+    lua_gettable(pState, -2);
+    field = lua_tolstring(pState, -1, &len);
+    lua_pop(pState, 1);
+    if (len) {
+        return field;
+    }
+    return "";
+}
+
+
+int getIntField(struct lua_State *pState, const char *name) {
+    int isnum = 0;
+    int result = 0;
+    lua_pushstring(pState, name);
+    lua_gettable(pState, -2);
+    result = (int)lua_tointegerx(pState, -1, &isnum);
+    lua_pop(pState, 1);
+    if (isnum) {
+        return result;
+    }
+    return 0;
+}
+
+
+void demoGetStringValueFromTable(struct lua_State *pState) {
+    assert(loadTable(pState, "conf = {name=\"ak47\", damage=33.3, clipSize=30}\n", "conf"));
+
+    // the pattern shown here is that, the getters keep the lua table at index -2 while
+    // pushing and popping the names of the field to and out of the stack; even if one
+    // getter fail to get the expected value from the table, it should still pop the
+    // field name to save space for the subsequent calls - it probably could be done in
+    // a RAII fashion
+    assert(std::string{"ak47"} == getStringField(pState, "name"));
+
+    // lua_tolstring() has a protection mechanism (&len)
+    assert(getStringField(pState, "_name").empty());
+
+    // number (double) is casted to string via lua_tolstring() function
+    assert(std::string{"33.3"} == getStringField(pState, "damage"));
+    lua_pop(pState, -1);
+}
+
+
+void demoGetIntegerFromTable(struct lua_State *pState) {
+    assert(loadTable(pState, "conf = {name=\"ak47\", damage=33.3, clipSize=30}\n", "conf"));
+    assert(!getIntField(pState, "_name"));
+
+    // the internal lua getter correctly identifies that, the value stored against [damage]
+    // field is a float (number); however it also decides that, since the caller wants to
+    // operate in integer mode, instead of rounding, it "rejects" the request
+    // the way it does the mode detection is entertaining
+    assert(0 == getIntField(pState, "damage"));
+    assert(30 == getIntField(pState, "clipSize"));
+}
+
+
+int main(int argc, char **argv) {
+    struct lua_State *pState = luaL_newstate();
+    luaL_openlibs(pState);
+
+    demoGetStringValueFromTable(pState);
+    demoGetIntegerFromTable(pState);
+
+    lua_close(pState);
+    return 0;
+}
---
+++ examples/tableop.h
@@ -0,0 +1,14 @@
+//
+// Created by wein on 12/18/17.
+//
+
+#ifndef LUA_TABLEOP_H
+#define LUA_TABLEOP_H
+
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+};
+
+#endif //LUA_TABLEOP_H
