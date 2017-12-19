75a36e297d11d21e37322a024a740baa7cde569a
Wei Ning <nwcgnw@gmail.com>
2017-12-20 02:00:28 +1300

    - add new example: call-lua-funcs.cpp

---
+++ examples/callluafuncs.h
@@ -0,0 +1,14 @@
+//
+// Created by wein on 12/20/17.
+//
+
+#ifndef LUA_CALLLUAFUNCS_H
+#define LUA_CALLLUAFUNCS_H
+
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+};
+
+#endif //LUA_CALLLUAFUNCS_H
