25985660926422b0520b6817db439ff74c009205
Wei Ning <nwcgnw@gmail.com>
2017-12-21 12:33:00 +1300

    - add example implementation of exists(), isdir(), listdir() using boost::filesystem
    - show how to implement C function following the Lua function return pattern;
    
    - show how to return array (table with continuous numeric keys)

--- examples/cmodules/boost_interpreter.cpp
+++ examples/cmodules/boost_interpreter.cpp
@@ -4,6 +4,9 @@
 
 #include "boost_interpreter.h"
 
+#include <iostream>
+#include <sstream>
+
 #include <boost/filesystem.hpp>
 
 
@@ -23,23 +26,65 @@ static int isfile(struct lua_State *L) {
 }
 
 
+static int listdir(struct lua_State *L) {
+    const char *path = luaL_checkstring(L, 1);
+    std::stringstream errorMessage{};
+
+    if (!boost::filesystem::is_directory(path)) {
+
+        // this is one naive way to handle the soft exception,
+        // but I can do better than that, see the next block
+        //
+        // std::cerr << "Not a directory: " << path << std::endl;
+        // return 0;
+
+        // follow the Lua function return pattern;
+        // honorable mentioning: Golang
+        //
+        // result, err = listdir("/abc")
+        // print(err)
+        // Not a directory: /abc
+        // print(result)
+        // nil
+
+        errorMessage << "Not a directory: " << path;
+        lua_pushnil(L);
+        lua_pushstring(L, errorMessage.str().c_str());
+        return 2;
+    }
+
+    boost::filesystem::directory_iterator di{path};
+    boost::filesystem::directory_iterator end{};
+
+    lua_newtable(L);
+    for (int idx = 1; di != end; ++di, ++idx) {
+        lua_pushinteger(L, idx);
+        lua_pushstring(L, di->path().c_str());
+        lua_settable(L, -3);  // will pop the above values
+    }
+
+    return 1;
+}
+
 
 int main(int argc, char **argv) {
     char buff[255];
     int error;
-    struct lua_State *pState = luaL_newstate();
-    luaL_openlibs(pState);
-    lua_pushcfunction(pState, exists);
-    lua_setglobal(pState, "exists");
-    lua_pushcfunction(pState, isfile);
-    lua_setglobal(pState, "isfile");
+    struct lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    lua_pushcfunction(L, exists);
+    lua_setglobal(L, "exists");
+    lua_pushcfunction(L, isfile);
+    lua_setglobal(L, "isfile");
+    lua_pushcfunction(L, listdir);
+    lua_setglobal(L, "listdir");
     while (fgets(buff, sizeof(buff), stdin) != NULL) {
-        error = luaL_loadstring(pState, buff) || lua_pcall(pState, 0, 0, 0);
+        error = luaL_loadstring(L, buff) || lua_pcall(L, 0, 0, 0);
         if (error) {
-            fprintf(stderr, "%s\n", lua_tostring(pState, -1));
-            lua_pop(pState, 1);
+            fprintf(stderr, "%s\n", lua_tostring(L, -1));
+            lua_pop(L, 1);
         }
     }
-    lua_close(pState);
+    lua_close(L);
     return 0;
 }
