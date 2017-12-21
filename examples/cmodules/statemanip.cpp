389c3feafd0eb9126ff36e7c8571a21eadf684db
Wei Ning <nwcgnw@gmail.com>
2017-12-22 11:40:21 +1300

    - complete statemanip.cpp example that shows:
    how to manipulate Lua registry;
    
    how to manipulate the global table _G

--- examples/cmodules/statemanip.cpp
+++ examples/cmodules/statemanip.cpp
@@ -18,6 +18,62 @@ extern "C" {
 }
 
 
-extern "C" int luaopen_libstatemanip(lua_State *L) {
+namespace {
+
+int setGlobalField(lua_State *L) {
+    const char *key = luaL_checkstring(L, 1);
+    luaL_checkany(L, 2);
+    lua_getglobal(L, "_G");
+    if (!lua_istable(L, -1)) {
+        return 0;
+    }
+    lua_pushvalue(L, 2);
+
+    // TODO: verify the stack! Here is what I think the stack holds:
+    // [ v   -1 (4)
+    //   t   -2 (3)
+    //   v   -3 (2)
+    //   k ] -4 (1)
+    lua_setfield(L, -2, key);
     return 0;
+}
+
+int getGlobalField(lua_State *L) {
+    const char *key = luaL_checkstring(L, 1);
+    lua_getglobal(L, "_G");
+    if (!lua_istable(L, -1)) {
+        return 0;  // return nil
+    }
+    lua_getfield(L, -1, key);
+    return 1;
+}
+
+int setRegField(lua_State *L) {
+    const char *key = luaL_checkstring(L, 1);
+    luaL_checkany(L, 2);
+    lua_pushvalue(L, 2);
+    lua_setfield(L, LUA_REGISTRYINDEX, key);
+    return 0;
+}
+
+int getRegField(lua_State *L) {
+    const char *key = luaL_checkstring(L, 1);
+    lua_getfield(L, LUA_REGISTRYINDEX, key);
+    return 1;
+}
+
+const struct luaL_Reg module[] = {
+    {"setRegField", setRegField},
+    {"getRegField", getRegField},
+    {"setGlobalField", setGlobalField},
+    {"getGlobalField", getGlobalField},
+    {nullptr, nullptr}
+};
+
+}
+
+
+extern "C" int luaopen_libstatemanip(lua_State *L) {
+    luaL_newlib(L, module);
+    return 1;
 }
--- examples/cmodules/statemanip.lua
+++ examples/cmodules/statemanip.lua
@@ -4,9 +4,44 @@ local function initialize()
 end
 
 
+local function demoSetGetGlobalRegistryField()
+    local m = require "libstatemanip"
+    
+    -- we really should not use this function except for debugging.
+    assert(m.getRegField("_PRELOAD") == debug.getregistry()["_PRELOAD"])
+    
+    m.setRegField("level.map", "brecourt")
+    assert("brecourt" == m.getRegField("level.map"))
+    
+    local function foo()
+    end
+    m.setRegField("level.init", foo)
+    assert(foo == m.getRegField("level.init"))
+    
+    m.setRegField("level.numplayers", 30)
+    assert(30 == m.getRegField("level.numplayers"))
+end
+
+
+local function demoSetGetGlobalVariables()
+    local m = require "libstatemanip"
+        
+    -- if I name the field "level_maxtime" the value can be read from a 
+    -- global variable of the same name!! 
+    -- this looks like a side effect to me but recall how Lua 
+    -- handle scoping from the earlier chapter on variables
+    m.setGlobalField("level_maxtime", 13.13)
+    assert(13.13 == _G["level_maxtime"])
+    assert(13.13 == level_maxtime)
+    assert(13.13 == m.getGlobalField("level_maxtime"))
+end
+
+
 local function run()
     initialize()
     
+    demoSetGetGlobalRegistryField()
+    demoSetGetGlobalVariables()
 end
 
 
