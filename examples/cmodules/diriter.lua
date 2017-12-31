344a21c3d20e48f1d406ada936fd61d3b2c69bbd
Wei Ning <nwcgnw@gmail.com>
2017-12-31 21:34:07 +1300

    - complete diriter_c & diriter_cxx examples
    - diriter_cxx is flawed; the reason has been documented in the Lua source code - CXX RAII idiom doesn't work with Lua's userdata out of box.

--- examples/cmodules/diriter.lua
+++ examples/cmodules/diriter.lua
@@ -15,9 +15,21 @@ local function demoDirectoryIteratorC()
 end
 
 
+-- the CXX implementation is flawed
+-- the raw buffer, the "userdata", is completely managed by Lua, 
+-- therefore there is no chance for the CXX object destructor to run, 
+-- because Lua garbage collector does not even know this buffer  
+-- is "an object";
+-- CXX smart pointer can't help either; the RAII idiom relies on scope, 
+-- which is not how Lua GC works 
 local function demoDirectoryIteratorCXX()
     local m = require "libdiriter_cxx"
-    print(m)
+    local count = 0
+    
+    for fname in m.open("/tmp") do
+        count = count + 1
+    end
+    assert(count > 1)
 end
 
 
--- examples/cmodules/diriter_cxx.cpp
+++ examples/cmodules/diriter_cxx.cpp
@@ -3,13 +3,96 @@
 //
 
 
+#include <dirent.h>
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <memory>
+
 extern "C" {
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
 }
 
+namespace {
+
+class Dir {
+public:
+    /*
+     * wrapping C open() function
+     */
+    bool open(const char *path);
+    void close();
+    bool isValid();
+
+    /*
+     * This method manipulates Lua state, pushing the file name to
+     * its stack;
+     *
+     * @return bool: return false if there is no file name to fetch,
+     * this is to tell the caller that no string has been pushed to
+     * the stack
+     */
+    bool getNextFname(lua_State *L);
+
+private:
+    DIR *dir = nullptr;
+};
+
+bool Dir::open(const char *path) {
+    dir = opendir(path);
+    return bool(dir);
+}
+
+bool Dir::isValid() {
+    return dir != nullptr;
+}
+
+bool Dir::getNextFname(lua_State *L) {
+    if (isValid()) {
+        struct dirent *entry = readdir(dir);
+        if (entry) {
+            lua_pushstring(L, entry->d_name);
+            return true;
+        }
+    }
+    return false;
+}
+
+void Dir::close() {
+    if (dir) {
+        printf("calling Dir::close()\n");
+        closedir(dir);
+    }
+}
+
+int iterator(lua_State *L) {
+    auto pDir = (Dir *)lua_touserdata(L, lua_upvalueindex(1));
+    return pDir->getNextFname(L) ? 1 : 0;
+}
+
+int open(lua_State *L) {
+    const char *path = luaL_checkstring(L, 1);
+    auto pDir = (Dir *)lua_newuserdata(L, sizeof(Dir));
+    if (!pDir->open(path)) {
+        luaL_error(
+            L,
+            "Can not open %s: %s", path, strerror(errno));
+    }
+    lua_pushcclosure(L, iterator, 1);
+    return 1;
+}
+
+const struct luaL_Reg module[]{
+    {"open", open},
+
+    {nullptr, nullptr}
+};
+
+}
 
 extern "C" int luaopen_libdiriter_cxx(lua_State *L) {
-    return 0;
+    luaL_newlib(L, module);
+    return 1;
 }
